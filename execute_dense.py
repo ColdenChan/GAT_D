90632815c53b57c653cf8c4e261c904c3050c797
ColdenChan <654562715@qq.com>
2018-12-22 13:29:54 +0800

    add sh file

--- execute_dense.py
+++ execute_dense.py
@@ -1,16 +1,19 @@
 import time
-import scipy.sparse as sp
 import numpy as np
 import tensorflow as tf
+import os
+from models import GAT
+from utils import process
 import argparse
 
-from models import GAT
-from models import SpGAT
-from utils import process
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default='cora', type=str, help='choose a dataset: cora, citeseer, pubmed')
+parser.add_argument('--gpuid', default='0' ,type=str, help='choose a gpu to excute program:0, 1, 2, 3')
+opts = parser.parse_args()
 
-checkpt_file = 'pre_trained/cora/mod_cora.ckpt'
+checkpt_file = 'pre_trained/'+ opts.dataset +'/mod_'+ opts.dataset +'.ckpt'
 
-dataset = 'cora'
+dataset = opts.dataset    #cora, citeseer, pubmed
 
 # training params
 batch_size = 1
@@ -22,8 +25,7 @@ hid_units = [8] # numbers of hidden units per each attention head in each layer
 n_heads = [8, 1] # additional entry for the output layer
 residual = False
 nonlinearity = tf.nn.elu
-# model = GAT
-model = SpGAT
+model = GAT
 
 print('Dataset: ' + dataset)
 print('----- Opt. hyperparams -----')
@@ -37,16 +39,42 @@ print('residual: ' + str(residual))
 print('nonlinearity: ' + str(nonlinearity))
 print('model: ' + str(model))
 
-sparse = True
-
 adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = process.load_data(dataset)
-features, spars = process.preprocess_features(features)
+###
+#'cora':
+#   adj:2708*2708
+#   features:2708*1433
+#   y_train, y_val, y_test:2708*7; y_train:前140行有类标， y_val:140-639行有类标（500行）， y_test:最后1000行有类标
+#   train_mask, val_mask, test_mask:2708*1; train_mask:前140列为true, val_mask:140-639列为true（500列）, test_mask:最后1000列为true
+#
+#'citeseer':
+#   adj:3327*3327
+#   features:3327*3703
+#   y_train, y_val, y_test:2708*6; y_train:前120行有类标， y_val:120-619行有类标（500行）， y_test:最后1015行中的1000行有类标（there are some isolated nodes in the graph）
+#   train_mask, val_mask, test_mask:2708*1; train_mask:前120列为true, val_mask:120-619列为true（500列）, test_mask:最后1015列中的1000列为true
+#
+#'pubmed':
+#   adj:19717*19717
+#   features:19717*500
+#   y_train, y_val, y_test:19717*3; y_train:前60行有类标， y_val:60-559行有类标（500行）， y_test:最后1000行有类标
+#   train_mask, val_mask, test_mask:2708*1; train_mask:前60列为true, val_mask:60-559列为true（500列）, test_mask:最后1000列为true
+###
+features, spars = process.preprocess_features(features)     # featrues是一个规范化（每个元素除以该行和）的dense矩阵；
+                                                            # spars则是features的稀疏矩阵元组表示（非零元素的坐标，非零元素值，shape）
 
-nb_nodes = features.shape[0]
-ft_size = features.shape[1]
-nb_classes = y_train.shape[1]
+nb_nodes = features.shape[0]    #node个数
+ft_size = features.shape[1]     #feature个数
+nb_classes = y_train.shape[1]   #class个数
 
+adj = adj.todense() #dense邻接矩阵
+##tt = np.sum(adj>1) 结果都等于0  说明三个数据集的邻接矩阵都是0,1矩阵
+#tt = np.sum(adj.diagonal()) citeseer数据集的结果等于124 说明存在自循环
+#tt = np.sum(adj.diagonal()) pubmed数据集的结果等于3 说明存在自循环
+#tt = np.where(adj.diagonal()==1)
+
+#增加一维，计算conv1d
 features = features[np.newaxis]
+adj = adj[np.newaxis]
 y_train = y_train[np.newaxis]
 y_val = y_val[np.newaxis]
 y_test = y_test[np.newaxis]
@@ -54,41 +82,37 @@ train_mask = train_mask[np.newaxis]
 val_mask = val_mask[np.newaxis]
 test_mask = test_mask[np.newaxis]
 
-if sparse:
-    biases = process.preprocess_adj_bias(adj)   #稀疏邻接矩阵+自循环，对角线和每个节点对应的一阶领域置为1
-else:
-    adj = adj.todense()
-    adj = adj[np.newaxis]
-    biases = process.adj_to_bias(adj, [nb_nodes], nhood=1)
+biases = process.adj_to_bias(adj, [nb_nodes], nhood=1)  #mask: 对角线和每个节点对应的一阶领域置为0，其余的置为-1e9
 
-with tf.Graph().as_default():
+os.environ["CUDA_VISIBLE_DEVICES"] = opts.gpuid
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.allow_soft_placement = True
+config.log_device_placement = False
+with tf.Graph(config=config).as_default():
+#with tf.Graph().as_default():
     with tf.name_scope('input'):
         ftr_in = tf.placeholder(dtype=tf.float32, shape=(batch_size, nb_nodes, ft_size))    #feature matrix
-        if sparse:
-            #bias_idx = tf.placeholder(tf.int64)
-            #bias_val = tf.placeholder(tf.float32)
-            #bias_shape = tf.placeholder(tf.int64)
-            bias_in = tf.sparse_placeholder(dtype=tf.float32)   #稀疏tensor占位符，需传入稀疏tensor的indices, data, shape   #mask matrix
-        else:
-            bias_in = tf.placeholder(dtype=tf.float32, shape=(batch_size, nb_nodes, nb_nodes))
+        bias_in = tf.placeholder(dtype=tf.float32, shape=(batch_size, nb_nodes, nb_nodes))  #mask matrix
         lbl_in = tf.placeholder(dtype=tf.int32, shape=(batch_size, nb_nodes, nb_classes))   #class matrix
-        msk_in = tf.placeholder(dtype=tf.int32, shape=(batch_size, nb_nodes))               #mask vector
-        attn_drop = tf.placeholder(dtype=tf.float32, shape=())
-        ffd_drop = tf.placeholder(dtype=tf.float32, shape=())
+        msk_in = tf.placeholder(dtype=tf.int32, shape=(batch_size, nb_nodes))   #mask vector
+        attn_drop = tf.placeholder(dtype=tf.float32, shape=())  #attention dropout
+        ffd_drop = tf.placeholder(dtype=tf.float32, shape=())   #feedforword dropout
         is_train = tf.placeholder(dtype=tf.bool, shape=())
 
     logits = model.inference(ftr_in, nb_classes, nb_nodes, is_train,
                                 attn_drop, ffd_drop,
                                 bias_mat=bias_in,
                                 hid_units=hid_units, n_heads=n_heads,
-                                residual=residual, activation=nonlinearity)
-    log_resh = tf.reshape(logits, [-1, nb_classes])
-    lab_resh = tf.reshape(lbl_in, [-1, nb_classes])
-    msk_resh = tf.reshape(msk_in, [-1])
-    loss = model.masked_softmax_cross_entropy(log_resh, lab_resh, msk_resh)
-    accuracy = model.masked_accuracy(log_resh, lab_resh, msk_resh)
+                                residual=residual, activation=nonlinearity) #shape=(batch_size, nb_nodes, nb_classes)
 
-    train_op = model.training(loss, lr, l2_coef)
+    log_resh = tf.reshape(logits, [-1, nb_classes]) #shape=(batch_size*nb_nodes, nb_classes)
+    lab_resh = tf.reshape(lbl_in, [-1, nb_classes]) #shape=(batch_size*nb_nodes, nb_classes)
+    msk_resh = tf.reshape(msk_in, [-1])             #shape=(batch_size*nb_nodes,)
+    loss = model.masked_softmax_cross_entropy(log_resh, lab_resh, msk_resh) #交叉熵损失
+    accuracy = model.masked_accuracy(log_resh, lab_resh, msk_resh)  #分类准确率
+
+    train_op = model.training(loss, lr, l2_coef)    #tf.train.AdamOptimizer(learning_rate=lr).minimize(loss+lossL2)
 
     saver = tf.train.Saver()
 
@@ -106,20 +130,17 @@ with tf.Graph().as_default():
         val_loss_avg = 0
         val_acc_avg = 0
 
+        #nb_epochs = 100000
         for epoch in range(nb_epochs):
+            #trainning
             tr_step = 0
-            tr_size = features.shape[0]
+            tr_size = features.shape[0] #=1; features.shape: (1,nb_nodes,ft_size)
 
-            while tr_step * batch_size < tr_size:
-                if sparse:
-                    bbias = biases
-                else:
-                    bbias = biases[tr_step*batch_size:(tr_step+1)*batch_size]
-
+            while tr_step * batch_size < tr_size:   #batch_size = 1
                 _, loss_value_tr, acc_tr = sess.run([train_op, loss, accuracy],
                     feed_dict={
                         ftr_in: features[tr_step*batch_size:(tr_step+1)*batch_size],
-                        bias_in: bbias,
+                        bias_in: biases[tr_step*batch_size:(tr_step+1)*batch_size],
                         lbl_in: y_train[tr_step*batch_size:(tr_step+1)*batch_size],
                         msk_in: train_mask[tr_step*batch_size:(tr_step+1)*batch_size],
                         is_train: True,
@@ -128,18 +149,15 @@ with tf.Graph().as_default():
                 train_acc_avg += acc_tr
                 tr_step += 1
 
+            #validating: without train_op
             vl_step = 0
             vl_size = features.shape[0]
 
             while vl_step * batch_size < vl_size:
-                if sparse:
-                    bbias = biases
-                else:
-                    bbias = biases[vl_step*batch_size:(vl_step+1)*batch_size]
                 loss_value_vl, acc_vl = sess.run([loss, accuracy],
                     feed_dict={
                         ftr_in: features[vl_step*batch_size:(vl_step+1)*batch_size],
-                        bias_in: bbias,
+                        bias_in: biases[vl_step*batch_size:(vl_step+1)*batch_size],
                         lbl_in: y_val[vl_step*batch_size:(vl_step+1)*batch_size],
                         msk_in: val_mask[vl_step*batch_size:(vl_step+1)*batch_size],
                         is_train: False,
@@ -153,7 +171,7 @@ with tf.Graph().as_default():
                     val_loss_avg/vl_step, val_acc_avg/vl_step))
 
             if val_acc_avg/vl_step >= vacc_mx or val_loss_avg/vl_step <= vlss_mn:
-                if val_acc_avg/vl_step >= vacc_mx and val_loss_avg/vl_step <= vlss_mn:
+                if val_acc_avg/vl_step >= vacc_mx and val_loss_avg/vl_step <= vlss_mn:  #验证集准确率提升并且验证集损失降低，则存储模型相关参数
                     vacc_early_model = val_acc_avg/vl_step
                     vlss_early_model = val_loss_avg/vl_step
                     saver.save(sess, checkpt_file)
@@ -162,7 +180,7 @@ with tf.Graph().as_default():
                 curr_step = 0
             else:
                 curr_step += 1
-                if curr_step == patience:
+                if curr_step == patience:   #patience = 100
                     print('Early stop! Min loss: ', vlss_mn, ', Max accuracy: ', vacc_mx)
                     print('Early stop model validation loss: ', vlss_early_model, ', accuracy: ', vacc_early_model)
                     break
@@ -174,20 +192,17 @@ with tf.Graph().as_default():
 
         saver.restore(sess, checkpt_file)
 
+        #testing
         ts_size = features.shape[0]
         ts_step = 0
         ts_loss = 0.0
         ts_acc = 0.0
 
         while ts_step * batch_size < ts_size:
-            if sparse:
-                bbias = biases
-            else:
-                bbias = biases[ts_step*batch_size:(ts_step+1)*batch_size]
             loss_value_ts, acc_ts = sess.run([loss, accuracy],
                 feed_dict={
                     ftr_in: features[ts_step*batch_size:(ts_step+1)*batch_size],
-                    bias_in: bbias,
+                    bias_in: biases[ts_step*batch_size:(ts_step+1)*batch_size],
                     lbl_in: y_test[ts_step*batch_size:(ts_step+1)*batch_size],
                     msk_in: test_mask[ts_step*batch_size:(ts_step+1)*batch_size],
                     is_train: False,
---
+++ sh_execute_sparse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+#####################################################
+# Copyright (c) 2018 USTC, Inc. All Rights Reserved
+#####################################################
+# File:    sh_execute_sparse.sh
+# Author:  cb
+# Date:    2018/12/20 14:21:42
+# Brief:
+#####################################################
+
+int=1
+end=&{1}
+while(( &int<=&end ))
+do
+	time=`date "+%Y%m%d%H%M%S"`
+	python execute_sparse.py --dataset &{2} --gpuid &{3} > ./log/cora/${time}.log 2>&1 &
+	sleep 1
+	let "int++"
+done
+
+# vim: set expandtab ts=4 sw=4 sts=4 tw=100
