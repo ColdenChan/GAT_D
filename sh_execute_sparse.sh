59769ea068741d20b3aa1e16835ee5760f1dc625
ColdenChan <654562715@qq.com>
2018-12-22 14:00:46 +0800

    modify sh file

--- sh_execute_sparse.sh
+++ sh_execute_sparse.sh
@@ -9,13 +9,13 @@
 # Brief:
 #####################################################
 
-declare -i i=1
-while(( i<=${1} ))
+i=1
+while(( i <= $1 ))
 do
 	time=`date "+%Y%m%d%H%M%S"`
 	python execute_sparse.py --dataset ${2} --gpuid ${3} > ./log/cora/${time}.log 2>&1 &
 	sleep 1
-	let ++i
+	let i++
 done
 
 # vim: set expandtab ts=4 sw=4 sts=4 tw=100
