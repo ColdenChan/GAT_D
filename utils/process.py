cec248c7629af3594156bf4368c4901fe9bd711e
ColdenChan <654562715@qq.com>
2018-12-27 21:17:34 +0800

    modify execute file

--- utils/process.py
+++ utils/process.py
@@ -19,7 +19,7 @@ def adj_to_bias(adj, sizes, nhood=1):
         mt[g] = np.eye(adj.shape[1])
         for _ in range(nhood):
             #mt[g] = np.matmul(mt[g], (adj[g] + np.eye(adj.shape[1])))      #单位矩阵*（邻接矩阵+单位矩阵）,所以该步有点多余,改为下句
-            mt[g] = np.matmul(adj[g], adj[g]) + adj[g] + np.eye(adj.shape[1])   #邻接矩阵+单位阵 （加了个自循环）
+            mt[g] = np.matmul(adj[g], adj[g])*0.1 + adj[g] + np.eye(adj.shape[1])   #邻接矩阵+单位阵 （加了个自循环）
         #经过前面验证，传进来的邻接矩阵是只有0和1的，再加上一个单位阵
         # for i in range(sizes[g]):
         #     for j in range(sizes[g]):
